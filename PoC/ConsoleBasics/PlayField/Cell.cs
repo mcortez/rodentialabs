6d1cb20eba2e4ab94566dec77bfcf798973d3341
Michael Cortez <mcortez@fullcoll.edu>
2017-06-09 18:13:51 -0700

    Added PlayField buffering proof of concept
    Lessons learned:
    * Updating the console is *slow*
      1. Need to minimize the number of times Console.Write() is called.  Writing a single character seems to have the same performance hit as writing an entire page
    
      2. Changing color requires splitting output into multiple writes. See #1

---
+++ PoC/ConsoleBasics/PlayField/Cell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PlayField
+{
+    public class Cell
+    {
+        public char Char
+        { get; set; }
+
+        public ConsoleColor CharForegroundColor
+        { get; set; }
+
+        public ConsoleColor CharBackgroundColor
+        { get; set; }
+
+        public Cell()
+        {
+            Char = ' ';
+            CharForegroundColor = ConsoleColor.White;
+            CharBackgroundColor = ConsoleColor.Black;
+        }
+    }
+}
---
+++ PoC/ConsoleBasics/PlayField/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayField
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int viewPortWidth = 140;
+            int viewPortHeight = 40;
+
+            Cell[,] playField = new Cell[viewPortWidth, viewPortHeight];
+            Array.Clear(playField, 0, playField.Length);
+            Console.SetWindowSize(viewPortWidth + 1, viewPortHeight + 1);
+
+            do
+            {
+
+                Random r = new Random();
+
+                Array consoleColors = Enum.GetValues(typeof(ConsoleColor));
+
+                for (int x = 0; x < viewPortWidth; x++)
+                {
+                    for (int y = 0; y < viewPortHeight; y++)
+                    {
+                        char c = (char)('0' + (char)(r.Next(26)));
+                        playField[x, y] = new Cell();
+                        playField[x, y].Char = c;
+
+                        //playField[x, y].CharForegroundColor = r.Next(2) == 0 ? ConsoleColor.Black : ConsoleColor.White;
+                    }
+                }
+
+                Console.SetCursorPosition(0, 0);
+
+                StringBuilder sb = new StringBuilder(viewPortWidth);
+                for (int y = 0; y < viewPortHeight; y++)
+                {
+                    for (int x = 0; x < viewPortWidth; x++)
+                    {
+                        if( Console.ForegroundColor != playField[x, y].CharForegroundColor 
+                            || Console.BackgroundColor != playField[x, y].CharBackgroundColor )
+                        {
+                            Console.Write(sb.ToString());
+                            Console.ForegroundColor = playField[x, y].CharForegroundColor;
+                            Console.BackgroundColor = playField[x, y].CharBackgroundColor;
+                            sb.Clear();
+                        }
+                        sb.Append(playField[x, y].Char);
+                    }
+                    sb.AppendLine();
+                }
+                Console.Write(sb.ToString());
+
+
+
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
